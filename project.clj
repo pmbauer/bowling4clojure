2fbe27b00546cf5e93ef667c21c6448c6a233ff9
Paul M Bauer <paul.michael.bauer@gmail.com>
2012-03-02 10:42:16 -0700

    bump dependency versions, add travis-ci

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject bowling4clojure "1.0.0-SNAPSHOT"
   :description "Katas in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.4.0"]])
